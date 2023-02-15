bebe2d3dd6da535752af2efb5903a00447c81ae4
Illia Khristinchenko <ikhristi@c2s7d22.42wolfsburg.de>
2023-02-15 16:36:23 +0100

    Com

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: ikhristi <ikhristi@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/09 14:53:41 by ikhristi          #+#    #+#              #
-#    Updated: 2023/02/09 14:56:56 by ikhristi         ###   ########.fr        #
+#    Updated: 2023/02/14 15:59:38 by ikhristi         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,4 +15,14 @@ SRC_SERVER = server.c
 SRC_CLIENT = client.c
 
 SERVER = server
-CLIENT
+CLIENT = client
+
+LIBFT = inc/libft/
+PRINTF = inc/printf/
+
+SRCS_DIR = ./
+OBJS_DIR = objs/
+
+OBJ_SERVER = $(SRC_SERVER:.c=.o)
+OBJ_CLIENT = $(SRC_CLIENT:.c=.o)
+OBJ_SERVER_PREFIX = $(addprefix $(OBJS_DIR), $(OBJ_SERVER))
--- server.c
+++ server.c
@@ -6,16 +6,25 @@
 /*   By: ikhristi <ikhristi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:42:02 by ikhristi          #+#    #+#             */
-/*   Updated: 2023/02/12 20:33:13 by ikhristi         ###   ########.fr       */
+/*   Updated: 2023/02/14 16:42:06 by ikhristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/server.h"
 
+void	ft_sighandler(int sig, siginfo_t *info, void *uncotext)
+{
+
+}
+
 int	main(void)
 {
-	struct sigaction	s_sigaction;
+	struct sigaction	sa;
 
 	printf("Server's PID is: %d\n", getpid());
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = &ft_sighandler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 
 }

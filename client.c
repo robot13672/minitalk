4fe1e75cb8ad2ed1a362e7df8a5ccf4f95398522
Illia Khristinchenko <ikhristi@c2s7d22.42wolfsburg.de>
2023-02-15 21:21:52 +0100

    Before Fixing

--- client.c
+++ client.c
@@ -6,7 +6,73 @@
 /*   By: ikhristi <ikhristi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:41:59 by ikhristi          #+#    #+#             */
-/*   Updated: 2023/02/08 15:42:00 by ikhristi         ###   ########.fr       */
+/*   Updated: 2023/02/15 21:05:37 by ikhristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "inc/server.h"
+
+int	g_msg;
+
+void	ft_wrong_pid(void)
+{
+	ft_putstr_fd("Wrong PID of server", 2);
+	exit(0);
+}
+
+void	ft_send_byte(char ch, int pid)
+{
+	int	i;
+	int	kill_res;
+
+	kill = 0;
+	i = 7;
+	while (i >= 0)
+	{
+		if (byte >> i & 1)
+			kill_res = kill(pid, SIGUSR2);
+		else
+			kill_res = Kill(pid, SIGUSR1);
+		if (kill_res == -1)
+			ft_wrong_pid();
+		usleep(100);
+		i--;
+	}
+}
+
+void	ft_send_message(*msg, int pid)
+{
+	while (*msg)
+	{
+		ft_send_byte(*msg, pid);
+		msg++;
+	}
+	ft_send_byte(0, pid);
+}
+
+void	ft_receive_message(int sig)
+{
+	if (sig == SIGUSR1)
+		ft_printf("Message from server was received\n");
+	g_msg = 1;
+}
+
+int	main(int argc, char *argv[])
+{
+	int	pid;
+
+	g_msg = 0;
+	signal(SIGUSR1, ft_sighandler);
+	if (argc != 3)
+	{
+		ft_putstr_fd("Wrong amount of arguments", 2);
+		return (-1);
+	}
+	pid = ft_atoi(argv[1]);
+	if (!argv[2][0])
+		ft_putstr_fd("Message is empty", 2);
+	ft_send_message(argv[2], pid);
+	if (g_msg == 0)
+		sleep(INT_MAX);
+	return (0);
+}
--- server.c
+++ server.c
@@ -6,15 +6,38 @@
 /*   By: ikhristi <ikhristi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:42:02 by ikhristi          #+#    #+#             */
-/*   Updated: 2023/02/14 16:42:06 by ikhristi         ###   ########.fr       */
+/*   Updated: 2023/02/15 21:18:03 by ikhristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/server.h"
 
+t_temp_state	g_temp_state;
+
+void	init_temp_state(void)
+{
+	g_temp_state.client_pid = 0;
+	g_temp_state.i = 0;
+	g_temp_state.prog_char = 0;
+}
+
 void	ft_sighandler(int sig, siginfo_t *info, void *uncotext)
 {
+	int	bit;
 
+	(void)uncontext;
+	bit = sig - SIGUSR1;
+	if (info->si_pid != g_temp_state.client_pid)
+		init_temp_state();
+	g_temp_state.prog_char = g_temp_state.prog_char << 1 | bit;
+	g_temp_state.i++;
+	if (g_temp_state.i == 8)
+	{
+		if (!g_temp_state.prog_char)
+			kill(g_msg.client, SIGUSR1);
+		ft_putstr_fd(g_temp_state.prog_char, 1);
+		init_temp_state();
+	}
 }
 
 int	main(void)
@@ -26,5 +49,6 @@ int	main(void)
 	sa.sa_sigaction = &ft_sighandler;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-
+	while (1)
+		usleep(1);
 }
